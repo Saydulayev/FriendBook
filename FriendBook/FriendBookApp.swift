9d50935356a084df76315cc7f04237aaeaf04b9f
Saydulayev <saydulayev.wien@gmail.com>
2024-12-17 10:12:56 +0100

    Initial Commit

---
+++ FriendBook/FriendBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendBookApp.swift
+//  FriendBook
+//
+//  Created by Saydulayev on 17.12.24.
+//
+
+import SwiftUI
+
+@main
+struct FriendBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
